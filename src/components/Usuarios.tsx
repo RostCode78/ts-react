352603fbb977163d7fca647890419af3345fa03e
RostCode78 <zeroxcode78@gmail.com>
2023-06-07 01:28:53 -0600

    Feat: Inicio de API

---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { reqResApi } from '../api/reqRes';
+
+const Usuarios = () => {
+
+    useEffect(() => {
+
+        // Llamado al API
+        reqResApi.get('/users')
+            .then( resp => {
+                console.log( resp.data.data );
+            })
+            .catch( console.log )
+
+    }, [])
+
+    return (
+        <>
+            <h3>Usuarios:</h3>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Usuarios;
